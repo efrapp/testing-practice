9c2c37e87f57a408e7ba7a4869aba1d89e3803bb
Efrain Pinto <efrapp@gmail.com>
2020-01-15 07:36:57 -0500

    Add more tests to the capitalize method

--- spec/index.test.js
+++ spec/index.test.js
@@ -2,4 +2,6 @@ const { capitalize } = require('../index');
 
 test('first character capitalized', () => {
   expect(capitalize('javascript')).toBe('Javascript');
+  expect(capitalize('0avascript')).toBe('0avascript');
+  expect(capitalize('Javascript')).toBe('Javascript');
 });
