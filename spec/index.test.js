02c2be77c5da478195a74c5e67bbe2fc2e45b85e
Efrain Pinto <efrapp@gmail.com>
2020-01-15 09:53:45 -0500

    Add failed tests for the calculator method

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require('../index');
+const { capitalize, reverseString, calculator } = require('../index');
 
 test('first character capitalized', () => {
   expect(capitalize('javascript')).toBe('Javascript');
@@ -12,4 +12,11 @@ test('string reversed', () => {
   expect(reverseString('Javascript the best language'))
     .toBe('egaugnal tseb eht tpircsavaJ');
   expect(reverseString(' ')).toBe(' ');
+});
+
+test('arithmetic operations', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+  expect(calculator.subract(10, 4)).toBe(6);
+  expect(calculator.divide(60, 3)).toBe(20);
+  expect(calculator.multiply(30, 7)).toBe(210);
 });
